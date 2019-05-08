65ab91829fb942cbc579295ecfd21cb759e611b5
=mutaito <=carlosm525@gmail.com>
2019-05-08 18:11:26 -0400

    Add new component

---
+++ src/components/AddPastebin/AddPastebin.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+// import List from '../List/List';
+// import NewItem from '../NewItem/NewItem';
+// import uuid from "uuid";
+
+// export const TodoStates = {
+//   UNDONE: false,
+//   DONE: true,
+// }
+export default class AddPastebin extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = {
+//       items: [],
+//       // newItem: "",
+//     }
+//   }
+
+//   // updateNewItem = (text) => {
+//   //   this.setState({
+//   //     newItem: text
+//   //   })
+//   // }
+
+//   addItem = (text) => {
+//     this.setState({
+//       items: [
+//         ...this.state.items,
+//         { id: uuid.v4(), text, status: TodoStates.UNDONE },
+//       ]
+//     })
+//   }
+
+//   updateItem = (item) => {
+//     const itemIndex = this.state.items.findIndex(i => i.id === item.id)
+//     this.setState({
+//       items: [
+//         ...this.state.items.slice(0, itemIndex),
+//         item,
+//         ...this.state.items.slice(itemIndex + 1),
+//       ]
+//     })
+//   }
+
+//   deleteItem = (itemId) => {
+//     const itemIndex = this.state.items.findIndex(i => i.id === itemId)
+//     this.setState({
+//       items: [
+//         ...this.state.items.slice(0, itemIndex),
+//         ...this.state.items.slice(itemIndex + 1),
+//       ]
+//     })
+//   }
+
+  render () {
+    return (
+      <section className="formulario">
+       Formulario:
+      </section>
+    )
+  }
+}
--- src/service/index.js
+++ src/service/index.js
@@ -7,3 +7,13 @@ export function getRedditTop(){
       return myJson;
     });
 }
+
+export function addPastebin(){
+  return fetch('https://www.reddit.com/top.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    return myJson;
+  });
+}
